5df485ef91f8ea6cf0223eb6573defb9153dccd1
Marius Perle <Marius@FamiliePerle.de>
2026-06-14 09:29:25 +0200

    add loading & cleanup generated code

---
+++ src/app/select-file/select-file.ts
@@ -0,0 +1,43 @@
+import { Component, inject, signal } from '@angular/core';
+import { form, FormField, FormRoot, pattern, required } from '@angular/forms/signals';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'ord-select-file',
+  imports: [FormRoot, FormField],
+  templateUrl: './select-file.html',
+  styleUrl: './select-file.css',
+})
+export class SelectFile {
+  private router = inject(Router)
+  private activatedRoute  = inject(ActivatedRoute);
+
+  myModel = signal({
+    link: '',
+  });
+
+  myForm = form(
+    this.myModel,
+    (schema) => {
+      required(schema.link);
+      pattern(
+        schema.link,
+        // source: https://regexbox.com/regex-templates/url
+        /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)$/,
+        { message: 'Link must be a valid URL.' },
+      );
+    },
+    {
+      submission: {
+        action: async (field) => {
+          await this.router.navigate([], {
+            relativeTo: this.activatedRoute,
+            queryParams: {
+              ordFile: field.link().value(),
+            }
+          })
+        },
+      },
+    },
+  );
+}
---
+++ src/app/viewer/viewer.ts
@@ -0,0 +1,52 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OrdDocument } from '@open-resource-discovery/specification';
+import { isArrayKey } from './ord.utils';
+import { JsonPipe } from '@angular/common';
+
+@Component({
+  selector: 'ord-viewer',
+  imports: [JsonPipe],
+  templateUrl: './viewer.html',
+  styleUrl: './viewer.css',
+})
+export class Viewer implements OnInit {
+  activatedRoute = inject(ActivatedRoute);
+  document = signal<OrdDocument | undefined>(undefined);
+  loading = signal(false);
+
+  ngOnInit() {
+    this.activatedRoute.queryParams.subscribe((params) => {
+      const ordFile: string = params['ordFile'];
+      if (!ordFile) return;
+      void this.loadData(ordFile);
+    });
+  }
+
+  async loadData(
+    baseUrl = 'https://raw.githubusercontent.com/MariusPerle/free-api/main/base.json',
+  ) {
+    this.loading.set(true);
+    const mainResponse = await fetch(baseUrl);
+    const referenceDoc: { openResourceDiscoveryV1: { url: string }[] } = await mainResponse.json();
+    const docs = await Promise.all(
+      referenceDoc.openResourceDiscoveryV1.map(async ({ url }) => {
+        const ordResponse = await fetch(url);
+        return (await ordResponse.json()) as OrdDocument;
+      }),
+    );
+
+    const mainDoc: OrdDocument = {
+      openResourceDiscovery: '1.16',
+    };
+    for (const doc of docs) {
+      for (const [key, value] of Object.entries(doc)) {
+        if (isArrayKey(key)) {
+          mainDoc[key] = [...(mainDoc[key] ?? []), ...value];
+        }
+      }
+    }
+    this.document.set(mainDoc);
+    this.loading.set(false);
+  }
+}
