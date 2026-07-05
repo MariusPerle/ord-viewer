fea7db171a7d923e055320cd7cefc4dd1813b975
Marius Perle <Marius@FamiliePerle.de>
2026-07-05 08:06:59 +0200

    add markdown decoding

--- src/app/viewer/resource/resource.ts
+++ src/app/viewer/resource/resource.ts
@@ -1,12 +1,13 @@
-import { Component, input, signal, ChangeDetectionStrategy } from '@angular/core';
+import { Component, input, signal, ChangeDetectionStrategy, inject } from '@angular/core';
 import {
   ApiResource,
   EventResource,
 } from '@open-resource-discovery/specification/dist/generated/spec/v1/types/Document';
+import { MarkdownPipe } from 'ngx-markdown';
 
 @Component({
   selector: 'ord-resource',
-  imports: [],
+  imports: [MarkdownPipe],
   templateUrl: './resource.html',
   styleUrl: './resource.css',
 })
